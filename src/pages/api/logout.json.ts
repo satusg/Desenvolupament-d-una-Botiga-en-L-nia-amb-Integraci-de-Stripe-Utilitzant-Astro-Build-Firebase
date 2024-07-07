eea515a64b2e07f8eb0d536ac83a259ca5c28549
satusg <gsat1111@gmail.com>
2024-07-07 12:12:11 +0200

    Logout Component creation

---
+++ src/pages/api/logout.json.ts
@@ -0,0 +1,20 @@
+import { type APIRoute } from "astro";
+import { auth } from "@/firebase/config";
+export const GET: APIRoute = async({ params, request }) => { 
+    if(auth.currentUser && !auth.currentUser.isAnonymous){
+        await auth.signOut();
+        return new Response(JSON.stringify({
+            message: 'User logged out successfully',
+        }), {
+            headers: { 'content-type': 'application/json' },
+        });
+    } 
+    return new Response(JSON.stringify({
+        message: 'No user logged in',
+    }), {
+        headers: {
+            'content-type': 'application/json',
+            'status': '401',
+        },
+    });
+}
